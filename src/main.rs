0166d1898b8f8f36fe81a281689c8be0ca7e945f
Paul Coroneos <coroneos@gmail.com>
2020-02-22 08:47:33 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use tcod::colors::*;
+use tcod::console::*;
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+const LIMIT_FPS: i32 = 50;
+
+struct Tcod {
+    root: Root,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
